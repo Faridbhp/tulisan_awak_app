3cb69bd16031f885bc171e55a27f081eefb73ba7
Faridbhp <farid.bhp9431@gmail.com>
2024-10-07 16:17:15 +0700

    add upddatetime

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -5,16 +5,18 @@ class Note {
   final String content;
   final bool isArsip;
   final bool isPinned;
+  final DateTime updateTime;
 
   Note(
       {required this.keyData,
       required this.title,
       required this.content,
+      required this.updateTime,
       this.isArsip = false,
       this.isPinned = false});
 
   @override
   String toString() {
-    return 'Note{key: $keyData, title: $title, content: $content, isArsip: $isArsip isPinned: $isPinned}';
+    return 'Note{key: $keyData, title: $title, content: $content, isArsip: $isArsip isPinned: $isPinned, updateTime: $updateTime}';
   }
 }
