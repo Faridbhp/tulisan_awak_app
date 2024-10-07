5c9a38a722455e6177bcec522bcb6e3bce76294a
Faridbhp <farid.bhp9431@gmail.com>
2024-10-07 14:07:54 +0700

    Create grid_card.dart

---
+++ lib/components/grid_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tulisan_awak_app/components/note_card.dart';
+import 'package:tulisan_awak_app/models/note.dart';
+
+class GridCard extends StatelessWidget {
+  final List<Note> allNotes; // Change to List<Note>
+  final int gridCount;
+  const GridCard(
+      {super.key,
+      required this.allNotes,
+      required this.gridCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: gridCount, // Contoh: tiga kartu per baris
+        childAspectRatio: 1, // Atur aspek rasio sesuai kebutuhan
+        crossAxisSpacing: 10, // Ruang antara kartu
+        mainAxisSpacing: 10, // Ruang antara baris
+      ),
+      itemCount: allNotes.length, // Menggunakan panjang daftar allNotes
+      itemBuilder: (context, index) {
+        final note = allNotes[index]; // Ambil catatan berdasarkan index
+        return NoteCard(
+          note: note,
+          index: index,
+        );
+      },
+    );
+  }
+}
