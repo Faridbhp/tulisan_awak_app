30e4374d7843b68dfdb461c70a5204a84c8285ac
Faridbhp <farid.bhp9431@gmail.com>
2024-10-18 22:52:29 +0700

    hide button grid when not phone width

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -26,61 +26,67 @@ class SearchBarCustom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: BorderRadius.circular(borderRadius),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2), // Warna bayangan
-            blurRadius: 2.0, // Besarnya blur pada bayangan
-            offset: Offset(0, 1), // Posisi bayangan (x, y)
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Container(
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(borderRadius),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2), // Warna bayangan
+                blurRadius: 2.0, // Besarnya blur pada bayangan
+                offset: Offset(0, 1), // Posisi bayangan (x, y)
+              ),
+            ],
           ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Builder(
-            builder: (context) {
-              return IconButton(
-                icon: Icon(
-                  Icons.menu,
-                  color: textColor,
-                  size: fontSize,
-                ),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer(); // Membuka drawer
+          child: Row(
+            children: [
+              Builder(
+                builder: (context) {
+                  return IconButton(
+                    icon: Icon(
+                      Icons.menu,
+                      color: textColor,
+                      size: fontSize,
+                    ),
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer(); // Membuka drawer
+                    },
+                  );
                 },
-              );
-            },
-          ),
-          Expanded(
-            child: TextField(
-              onChanged: onSearchChanged,
-              decoration: InputDecoration(
-                hintText: 'Search...',
-                hintStyle: TextStyle(color: textColor),
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.symmetric(vertical: 10),
               ),
-              style: TextStyle(
-                color: textColor,
-                fontSize: fontSize,
+              Expanded(
+                child: TextField(
+                  onChanged: onSearchChanged,
+                  decoration: InputDecoration(
+                    hintText: 'Search...',
+                    hintStyle: TextStyle(color: textColor),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  style: TextStyle(
+                    color: textColor,
+                    fontSize: fontSize,
+                  ),
+                ),
               ),
-            ),
+              if (constraints.maxWidth <= 500) ...[
+                IconButton(
+                  icon: Icon(
+                    gridCount == 1
+                        ? Icons.splitscreen_outlined
+                        : Icons.grid_view_outlined,
+                    color: textColor,
+                    size: fontSize,
+                  ),
+                  onPressed: onChangeGridCount,
+                )
+              ]
+            ],
           ),
-          IconButton(
-            icon: Icon(
-              gridCount == 1
-                  ? Icons.splitscreen_outlined
-                  : Icons.grid_view_outlined,
-              color: textColor,
-              size: fontSize,
-            ),
-            onPressed: onChangeGridCount,
-          )
-        ],
-      ),
+        );
+      },
     );
   }
 }
