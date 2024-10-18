340f0c1108b69dd4ec16fc128ee23489d172035b
Faridbhp <farid.bhp9431@gmail.com>
2024-10-18 18:07:22 +0700

    create grid_card_staggered

---
+++ lib/components/grid_card_staggered.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:tulisan_awak_app/components/note_card.dart';
+import 'package:tulisan_awak_app/redux/models/note.dart';
+
+class GridCardStaggered extends StatelessWidget {
+  final List<Note> allNotes; // Change to List<Note>
+  final int gridCount;
+  const GridCardStaggered(
+      {super.key, required this.allNotes, required this.gridCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return MasonryGridView.builder(
+        gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: gridCount, // Jumlah kolom
+        ),
+        mainAxisSpacing: 0, // Jarak antara item secara vertikal
+        crossAxisSpacing: 8, // Jarak antara item secara horizontal
+        itemCount: allNotes.length,
+        itemBuilder: (context, index) {
+          final note = allNotes[index];
+          return NoteCard(
+            note: note,
+            index: index,
+          );
+        },
+      );
+  }
+}
